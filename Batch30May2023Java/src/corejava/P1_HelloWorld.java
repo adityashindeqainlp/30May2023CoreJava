6c71310e10e6b9bc3bd1444aa20096ce4e36326f
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-06-22 08:43:58 -0400

    Commit 1 core java

---
+++ Batch30May2023Java/src/corejava/P1_HelloWorld.java
@@ -0,0 +1,18 @@
+package corejava;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 21-Jun-2023 5:18:24 pm
+ **/
+public class P1_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+
+		// HelloWorld -- > Class --> Pascal Naming Convention
+
+		// helloWorldIAmJava --> Method,variables --> Camel Case Convention
+	}
+
+}
