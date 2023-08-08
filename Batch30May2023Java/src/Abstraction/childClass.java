53d059fec8d941daf717d6d5c04676b4b5512d39
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-08 15:24:17 -0400

    Child class Commit

---
+++ Batch30May2023Java/src/Abstraction/childClass.java
@@ -0,0 +1,21 @@
+package Abstraction;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 03-Aug-2023 5:43:24 pm
+ **/
+//public class childClass implements parent1, parent2 {
+//
+//	@Override
+//	public void m1() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	public int m2() {
+//		// TODO Auto-generated method stub
+//
+//		
+//	}
+//
+//}
---
+++ Batch30May2023Java/src/Encapsulation/child.java
@@ -0,0 +1,13 @@
+package Encapsulation;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 04-Aug-2023 5:15:14 pm
+ **/
+public class child  {
+
+	public static void main(String[] args) {
+
+		parent.accessConstructor();
+	
+	}
+}
