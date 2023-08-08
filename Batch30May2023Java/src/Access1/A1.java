7a1356422d00878266f08b4a671d7f9458caae8a
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-08 15:22:08 -0400

    Parent 1 Update

---
+++ Batch30May2023Java/src/Access1/A1.java
@@ -0,0 +1,26 @@
+package Access1;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 28-Jul-2023 5:11:31 pm
+ **/
+public class A1 {
+
+	public static int a = 1000;
+	public int b = 2000;
+
+	public static void m1() {
+
+		System.out.println("Static Method From A1 ");
+	}
+
+	public void m2() {
+
+		System.out.println("Non static Method From A1");
+	}
+
+	public A1() {
+
+		System.out.println("Constructor of A1");
+	}
+
+}
