7515ba14fad74a27003841a8acaa8e9390b532b1
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-11 09:50:56 -0400

    Array in java new Literal Method

---
+++ Batch30May2023Java/src/ArrayInJava/Array2.java
@@ -0,0 +1,20 @@
+package ArrayInJava;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 10-Aug-2023 4:46:28 pm
+ **/
+public class Array2 {
+
+	public static void main(String[] args) {
+
+		// Array Literal Method
+
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int a : arr) {
+
+			System.out.println(a);
+		}
+	}
+
+}
