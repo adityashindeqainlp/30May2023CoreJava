8b832ca047a8de1b44d7ca2386d4110782598482
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-21 10:41:47 -0400

    Defining The String

---
+++ Batch30May2023Java/src/Strings/DefiningTheString.java
@@ -0,0 +1,33 @@
+package Strings;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 11-Aug-2023 4:54:00 pm
+ **/
+public class DefiningTheString {
+
+	
+	public static void main(String[] args) {
+		
+
+		//old approach
+		String a =  new String("TOM");
+		
+		System.out.println(a);
+		
+		// T - 0 , O - 1 , M - 2
+		
+		//Literal Approach 
+		String b = "Jerry";
+		b = "Scoby";
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
