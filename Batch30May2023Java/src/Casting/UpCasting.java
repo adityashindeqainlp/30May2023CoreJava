1ac448eb814b8d272244230ed95d9cba40bfe550
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-21 10:43:49 -0400

    Up casting

---
+++ Batch30May2023Java/src/Casting/UpCasting.java
@@ -0,0 +1,17 @@
+package Casting;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 18-Aug-2023 5:49:55 pm
+ **/
+public class UpCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte a = 10; // -128 to 127 // 1 bytes 
+
+		int b = a;  // 4 bytes 
+
+	}
+
+}
