750e741f425b231a3158ca12e542971a7e3464d1
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-21 17:17:58 -0400

    Up Casting

--- Batch30May2023Java/src/Casting/UpCasting.java
+++ Batch30May2023Java/src/Casting/UpCasting.java
@@ -8,9 +8,16 @@ public class UpCasting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Up Casting // Widening // Implicit Casting 
+		
 		byte a = 10; // -128 to 127 // 1 bytes 
-
 		int b = a;  // 4 bytes 
+		
+		int c = 1000; // 4 Bytes
+		double d = c; // 8 Bytes 
+		System.out.println(d);
+		
+		
 
 	}
 
